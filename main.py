5312216287df2a37e22fde2ea4381d8775add6dd
maneesha14w <maneesha14w@gmail.com>
2021-02-06 01:14:10 +0530

    get user_input and make it lowercase

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import random 
 import string
 
+
+user_input = input("Please enter 'g' to generate 'v' to verify: ").lower()
+print(user_input)
+
 for i in range (random.randint(5,15)):
     print('Num' , i)
