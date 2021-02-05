c4d015922affa728608c0ce399fe5bca6f38b1f8
maneesha14w <maneesha14w@gmail.com>
2021-02-06 01:31:30 +0530

    menu creation and num and upper and lower array generation

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import random 
 import string
 
+def generatePass():
+    for i in range (random.randint(0,9)):
+        num_arr = [].append(i)
+        str_upper_arr = [].append(random.choice(string.ascii_uppercase))
+        str_lower_arr = [].append(random.choice(string.ascii_lowercase))
+        
+def verifyPass():
+    print('')
 
-user_input = input("Please enter 'g' to generate 'v' to verify: ").lower()
-print(user_input)
 
-for i in range (random.randint(5,15)):
-    print('Num' , i)
+
+    while True:             
+        user_input = input("Please enter 'g' to generate 'v' to verify: ").lower()
+        if user_input == 'g':
+            generatePass()
+            break
+        elif user_input == 'v':
+            verifyPass()
+            break
+        else:
+            print("OOps, Please enter 'g' or 'v'")
+            continue
+        
